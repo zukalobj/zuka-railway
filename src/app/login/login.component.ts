259ddc0f2d41d2e3745a13c5cdd63665fd9fdcf8
Default <zukalo123@gmail.com>
2025-09-05 12:03:49 -0700

    colors

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,16 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule,FormsModule,ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {  
+ loginForm: FormGroup;
+  loginFailed = false;
 
-
-  logIn(){
-    localStorage.setItem('token', 'token');
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
   }
-  }
 
+  onLogin() {
+    const { username, password } = this.loginForm.value;
+
+    // რეალური ავტორიზაცია ჩასვი აქ
+    if (username === 'admin' && password === '1234') {
+      this.loginFailed = false;
+      // გადამისამართება ან localStorage
+      alert('შესვლა წარმატებულია ✅');
+    } else {
+      this.loginFailed = true;
+    }
+  }
+}
