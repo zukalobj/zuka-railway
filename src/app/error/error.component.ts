427861dafdda99307762d784a377ecf284b95bdc
Default <zukalo123@gmail.com>
2025-09-15 23:54:52 -0700

    error

--- src/app/error/error.component.ts
+++ src/app/error/error.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-error',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './error.component.html',
   styleUrl: './error.component.scss'
 })
